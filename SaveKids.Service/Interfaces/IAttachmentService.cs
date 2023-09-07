fc7bc51e0074fc562018d29aaa543ef8df6c5204
Nurullo <ghostone234qwer.gmail.com>
2023-09-07 18:31:36 +0500

    Create all interfaces

---
+++ SaveKids.Service/Interfaces/IAttachmentService.cs
@@ -0,0 +1,10 @@
+﻿using SaveKids.Domain.Entities.Attachments;
+using SaveKids.Service.DTOs.Attachments;
+
+namespace SaveKids.Service.Interfaces;
+
+public interface IAttachmentService
+{
+    Task<Attachment> UploadAsync(AttachmentCreationDto dto);
+    Task<bool> RemoveAsync(Attachment attachment);
+}
