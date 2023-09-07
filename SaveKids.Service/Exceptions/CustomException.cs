5e752e15eec0cba5fc34da160acd6afb46197162
Nurullo <ghostone234qwer.gmail.com>
2023-09-07 18:04:26 +0500

    Added extensions and exceptions

---
+++ SaveKids.Service/Exceptions/CustomException.cs
@@ -0,0 +1,12 @@
+﻿namespace SaveKids.Service.Exceptions;
+
+public class CustomException : Exception
+{
+    public CustomException(string message) : base(message)
+    { }
+
+    public CustomException(string message, Exception innerException) : base(message, innerException)
+    { }
+
+    public int StatusCode { get; set; } = 422;
+}
---
+++ SaveKids.Service/Extensions/CollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using SaveKids.Domain.Configurations;
+
+namespace SaveKids.Service.Extensions;
+
+public static class CollectionExtension
+{
+    public static IQueryable<T> ToPaginate<T>(this IQueryable<T> values, PaginationParams pagination)
+    {
+        var source = values.Skip((pagination.PageIndex - 1) * pagination.PageSize).Take(pagination.PageSize);
+        return source;
+    }
+}
