6f983a6eb4858c7d3a530db5f80e23d4b5df2535
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-09-07 15:43:06 +0500

    ChangeAuditable

--- SaveKids.Domain/Entities/Auditable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SaveKids.Domain.Entities;
-
-public class Auditable
-{
-    public long Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
-    public bool IsDeleted { get; set; }
-}
