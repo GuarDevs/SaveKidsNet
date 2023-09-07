0a7fcd7f480c49bbe2b8236aed7466888ef7f173
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-09-07 18:41:17 +0500

    Add Services

---
+++ SaveKids.Service/Services/AttachmentService.cs
@@ -0,0 +1,18 @@
+﻿using SaveKids.Domain.Entities.Attachments;
+using SaveKids.Service.DTOs.Attachments;
+using SaveKids.Service.Interfaces;
+
+namespace SaveKids.Service.Services;
+
+public class AttachmentService : IAttachmentService
+{
+    public Task<bool> RemoveAsync(Attachment attachment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Attachment> UploadAsync(AttachmentCreationDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
