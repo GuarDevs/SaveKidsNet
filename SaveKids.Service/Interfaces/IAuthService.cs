5a086ecf9e098939b513d34898c7eb9a3d9ff034
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-09-08 12:47:32 +0500

    Add Jwt Authorization

---
+++ SaveKids.Service/Interfaces/IAuthService.cs
@@ -0,0 +1,6 @@
+﻿namespace SaveKids.Service.Interfaces;
+
+public interface IAuthService
+{
+    Task<string> GenerateTokenAsync(string telNumber, string password);
+}
--- SaveKids.WebApi/Extentions/ServiceCollection.cs
+++ SaveKids.WebApi/Extentions/ServiceCollection.cs
@@ -1,8 +1,13 @@
-﻿using SaveKids.DAL.IRepositories;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using SaveKids.DAL.IRepositories;
 using SaveKids.DAL.Repositories;
 using SaveKids.Service.Interfaces;
 using SaveKids.Service.Mappers;
 using SaveKids.Service.Services;
+using System.Text;
 
 namespace SaveKids.WebApi.Extentions;
 
@@ -17,7 +22,62 @@ public static class ServiceCollection
         services.AddScoped<ICrimeCategoryService, CrimeCategoryService > ();
         services.AddScoped<ICriminalService, CriminalService > ();
         services.AddScoped<IUserService, UserService > ();
+        services.AddScoped<IAuthService, AuthService>();
 
         services.AddAutoMapper(typeof(MappingProfile));
+    }
+
+    public static void AddJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(o =>
+        {
+            var key = Encoding.UTF8.GetBytes(configuration["JWT:Key"]);
+            o.SaveToken = true;
+            o.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = configuration["JWT:Issuer"],
+                ValidAudience = configuration["JWT:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            };
+        });
+    }
+
+
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(setup =>
+        {
+            // Include 'SecurityScheme' to use JWT Authentication
+            var jwtSecurityScheme = new OpenApiSecurityScheme
+            {
+                BearerFormat = "JWT",
+                Name = "JWT Authentication",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+            setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { jwtSecurityScheme, Array.Empty<string>() }
+                });
+        });
     }
 }
